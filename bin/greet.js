12c7a7de8e7be3ecdba087d9ffe9963db1391365
Zehan Zhao <cnallenzhao@gmail.com>
2015-01-06 14:35:22 +0800

    Changing to coffeescript

--- bin/greet.js
+++ bin/greet.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var greet = require('../index.js');
+var greet = require('../lib/index.js');
 var parseArgs = require('minimist')(process.argv.slice(2));
 
 console.log(greet(parseArgs._,parseArgs.drunk));
---
+++ src/index.coffee
@@ -0,0 +1,3 @@
+greet = (name, drunk) ->
+    if drunk then "Hello " + name + ", you look sexy today." else "Hello " + name
+module.exports = greet
